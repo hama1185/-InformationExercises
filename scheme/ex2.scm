9f856cf7e85880bff0455b46bafcee2bcc15c366
hama1185 <hamataku0120@gmail.com>
2020-07-31 06:41:11 +0900

    search関数をgetcousinで繰り返すタイプ

--- scheme/ex2.scm
+++ scheme/ex2.scm
@@ -1,12 +1,33 @@
 (define alphabet
   (read (open-input-file "./w2/alphabet")))
+
 (define tokugawa
   (read (open-input-file "./w2/tokugawa")))
+
 (define get-depth
   (lambda (tree n)
     (if (> n 0)
       (apply append (map (lambda (t) (get-depth t (- n 1))) (cdr tree)))
       (list(car tree))
+    )
+  )
+)
+
+(define search
+  (lambda (tree key n)
+    (if (equal? (car tree) key)
+      (n)
+      (0)
+    )
+  )
+)
+
+(define get-cousin
+  (lambda (tree key)
+    (if (null? (cdr tree))
+      
+      (list(car tree))
+      (map (lambda (t) (search t key (+ n 1))) (cdr tree))
     )
   )
 )
