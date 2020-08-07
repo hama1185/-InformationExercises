e0c4de816c862133f22b04e4690e2f758b0b883d
hama1185 <hamataku0120@gmail.com>
2020-08-07 20:46:04 +0900

    演習4完成

--- scheme/ex4.scm
+++ scheme/ex4.scm
@@ -1,52 +1,65 @@
-;課題4
-;今までのcons,car,cdrにstream-を追加．
 (define-syntax stream-cons
     (syntax-rules ()
         ((_ x y) (cons x (delay y)))
-    ))
+    )
+)
 
 (define-syntax stream-car
     (syntax-rules ()
         ((_ x) (car x ))
-    ))
+    )
+)
 
 (define-syntax stream-cdr
     (syntax-rules ()
         ((_ x) (force (cdr x)))
-    ))
+    )
+)
 
 ;2からはじまる整数の無限リストを生成する関数
-(define numbers (lambda ()
-  (letrec ((stream
-              (lambda (n) (stream-cons n (stream (+ n 1))))
-          ))
-          (stream 2))))
+(define numbers 
+    (lambda ()
+        (letrec (
+            (stream
+                    (lambda (n) (stream-cons n (stream (+ n 1))))
+            )
+                )
+            (stream 2)
+        )
+    )
+)
 
 ;無限リストの先頭から指定された個数の値を取り出す関数
-(define head (lambda (n L)
-  (if (<= n 0) '()
-      (cons (stream-car L) (head (- n 1) (stream-cdr L)))
-      )))
+(define head 
+    (lambda (n L)
+        (if (<= n 0) '()
+            (cons (stream-car L) (head (- n 1) (stream-cdr L)))
+        )
+    )
+)
 
 (define screen
-  (lambda (num L)
-    (if (= (modulo (stream-car L) num) 0) (screen num (stream-cdr L))
-     (stream-cons (stream-car L) (screen num (stream-cdr L))) ;else(割り切れない時)
+    (lambda (num L)
+        (if (= (modulo (stream-car L) num) 0) (screen num (stream-cdr L))
+            (stream-cons (stream-car L) (screen num (stream-cdr L))) ;else(割り切れない時)
+        )
     )
-))
+)
 
 (define EraList
-  (lambda (L)
-    (stream-cons (stream-car L) (EraList(screen (stream-car L) (stream-cdr L))))
-))
+    (lambda (L)
+        (stream-cons (stream-car L) (EraList(screen (stream-car L) (stream-cdr L))))
+    )
+)
 
 (define Eratosthenes
-  (lambda (N)
-    (cond ((= N 0) '())
-          ((= N 1) '(2))
-          (else (head N (EraList(numbers))))
+    (lambda (N)
+        (cond   ((= N 0) '())
+                ((= N 1) '(2))
+                (else (head N (EraList(numbers))))
+        )
     )
-))
+)
 
 (Eratosthenes 3)
 (Eratosthenes 10)
